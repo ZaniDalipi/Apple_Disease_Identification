602001c2ed9ee720bb4724f0b1e7ecddbb20132a
Zano <kranje_z@live.com>
2020-03-15 12:18:43 +0100

    fully functional commit sign up with google account

--- app/src/main/java/com/zanoapp/applediseaseindentificator/uiController/authenticationFirebase/SignUpFragment.kt
+++ app/src/main/java/com/zanoapp/applediseaseindentificator/uiController/authenticationFirebase/SignUpFragment.kt
@@ -8,36 +8,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.*
-
 import com.zanoapp.applediseaseindentificator.R
 import com.zanoapp.applediseaseindentificator.databinding.SignUpFragmentBinding
-import com.zanoapp.applediseaseindentificator.uiController.authenticationFirebase.SignUpViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
-private const val RC_SIGN_IN = 9001
-private const val TAG = "FirebaseUserControl"
 
 class SignUpFragment : Fragment() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: SignUpFragmentBinding
-    private lateinit var viewModel: SignUpViewModel
-    private val uiScope = CoroutineScope(Dispatchers.Main)
     private var account: GoogleSignInAccount? = null
 
     override fun onCreateView(
@@ -79,37 +66,11 @@ class SignUpFragment : Fragment() {
 
     }
 
-    fun signOutCurrentUser() {
-
-        val currentUser = account
-        currentUser?.isExpired
-        Toast.makeText(activity, "you have logged out : ${currentUser?.displayName}", Toast.LENGTH_SHORT).show()
-        binding.signOutButton.visibility = View.GONE
-        binding.signUpButton.visibility = View.VISIBLE
-        binding.userInfoTextView.text = "You have signed out Please signin with google "
-    }
-
 
     override fun onStart() {
         super.onStart()
-
-
-        if (account != null) {
-            binding.signUpButton.visibility = Button.GONE
-            binding.signOutButton.visibility = View.VISIBLE
-            } else {
-            binding.signUpButton.visibility = Button.VISIBLE
-            binding.signOutButton.visibility = View.GONE
-        }
-
-        if (binding.userInfoTextView != null) {
-            binding.userInfoTextView.text =
-                "current user logged in is ${account?.email}"
-            Toast.makeText(context, "Welcome ${account?.email}", Toast.LENGTH_LONG).show()
-        } else {
-            binding.userInfoTextView.text = getString(R.string.no_user_signed_in)
-        }
-
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 
     private fun signIn() {
@@ -120,55 +81,79 @@ class SignUpFragment : Fragment() {
         )
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun signOutCurrentUser() {
 
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            account = completedTask.getResult(ApiException::class.java)
-            account?.let {
-                Log.d(TAG, "firebaseAuthWithGoogle ${account!!.id}")
-                val credentials = GoogleAuthProvider.getCredential(account!!.idToken, null)
-                auth.signInWithCredential(credentials).addOnCompleteListener {
-                    if (completedTask.isSuccessful) {
-                        Toast.makeText(
-                                activity,
-                                "Succesfully signed in ${account!!.email}",
-                                Toast.LENGTH_SHORT
-                            )
-                            .show()
-                        account?.let { updateUI(account) }
-                        //findNavController().navigate(R.id.action_signUpFragment_to_cameraFragment)
-                    }
-                }
-            }
-        } catch (e: ApiException) {
-            Toast.makeText(activity, "Signing not succesfull", Toast.LENGTH_SHORT).show()
-            Snackbar.make(view!!, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+        auth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener {
+            Toast.makeText(
+                activity,
+                "you have logged out : ${account?.displayName} ",
+                Toast.LENGTH_SHORT
+            ).show()
+            binding.signOutButton.visibility = View.GONE
+            binding.signUpButton.visibility = View.VISIBLE
+            binding.userInfoTextView.text =
+                getString(R.string.after_sign_out_msg)
             updateUI(null)
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-    @SuppressLint("SetTextI18n")
-    fun updateUI(account: GoogleSignInAccount?) {
-
-        binding.signOutButton.visibility = View.VISIBLE
-        if (account != null) {
-            binding.signUpButton.visibility = View.GONE
-            binding.userInfoTextView.text = "Current User logged in : ${account.email.toString()}"
-            Toast.makeText(
-                activity,
-                "Welcome ${account?.email.toString()}",
-                Toast.LENGTH_SHORT
-            ).show()
+        if (requestCode == RC_SIGN_IN) {
+            val completedTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                account = completedTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+                updateUI(null)
+            }
         }
     }
 
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogle: ${account?.id}")
+
+        val credentials = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth.signInWithCredential(credentials)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        activity,
+                        "Successfully signed in : ${account?.email}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Snackbar.make(requireView(), "Authentication Failed.", Snackbar.LENGTH_SHORT)
+                        .show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateUI(user: FirebaseUser?) {
+
+        if (user != null) {
+            binding.userInfoTextView.text = """current user signed in is : ${user.email}"""
+
+            binding.signUpButton.visibility = View.GONE
+            binding.signOutButton.visibility = View.VISIBLE
+        } else {
+            binding.userInfoTextView.text = getString(R.string.no_user_signed_in)
+
+            binding.signUpButton.visibility = View.VISIBLE
+            binding.signOutButton.visibility = View.GONE
+        }
+
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+        private const val TAG = "FirebaseUserControl"
+    }
 }
